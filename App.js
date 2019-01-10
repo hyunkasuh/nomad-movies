7006013383eb354f0ec26d19b639c3030bd9c094
HyunkaSuh <sgh415@gmail.com>
2019-01-09 21:45:04 -0600

    ~#1-3 Constants and Screens set up

--- App.js
+++ App.js
@@ -1,13 +1,45 @@
 import React from 'react';
+import { AppLoading, Font } from "expo";
 import { StyleSheet, Text, View } from 'react-native';
+import { Ionicons } from "@expo/vector-icons";
+import Asset from 'expo-asset/src/Asset';
 
 export default class App extends React.Component {
+  state = {
+    loaded: false
+  };
+
+  handleError = (error) => console.log(error);
+
+  handleLoaded = () => this.setState({ loaded: true });
+
+  loadeAssets = async () => {
+    throw new Error("Im hungry!!!");
+    await Font.loadAsync({
+      ...Iconicons.font
+    });
+    // await Asset.loadAsync([
+    //   require("images/icon.png")
+    // ])
+  };
+
   render() {
-    return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-      </View>
-    );
+    const { loaded } = this.state;
+    if (loaded) {
+      return (
+        <View style={styles.container}>
+          <Text>Open up App.js to start working on your app!</Text>
+        </View>
+      );
+    } else {
+      return (
+        <AppLoading
+          startAsync={this.loadAssets}
+          onFinish={this.handleLoaded}
+          onError={this.handleError}
+        />
+      );
+    }
   }
 }
 
--- README.md
+++ README.md
@@ -2,3 +2,9 @@
 
 iOS / Android Movie Discovery App built with React Native.
 
+## Screens
+
+- [ ] Movies
+- [ ] TV Shows
+- [ ] Search
+- [ ] Detail
