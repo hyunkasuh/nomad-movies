7b6c53507b53b68b7fde59a3aa4e20752415aa65
HyunkaSuh <sgh415@gmail.com>
2019-01-13 22:58:18 -0600

    ~#4-4 TV Container

--- screens/Movies/MoviesContainer.js
+++ screens/Movies/MoviesContainer.js
@@ -1,12 +1,52 @@
 import React from "react";
 import MoviesPresenter from "./MoviesPresenter";
+import { movies } from "../../api";
 
 export default class extends React.Component { //React.Component 대신에 Component
     state = {
-        loading: true
+        loading: true,
+        upcoming: null,
+        popular: null,
+        nowPlaying: null,
+        error: null
     };
+
+    async componentDidMount() {
+        let upcoming, popular, nowPlaying, error;
+        try {
+            ({
+                data: { results: upcoming }
+            } = await movies.getUpcoming());
+            ({
+                data: { results: popular }
+            } = await movies.getPopular());
+            ({
+                data: { results: nowPlaying }
+            } = await movies.getNowPlaying());
+        } catch (error) {
+            console.log(error);
+            error = "Can't get Movies.";
+        } finally {
+            this.setState({
+                loading: false,
+                error,
+                upcoming,
+                popular,
+                nowPlaying
+            });
+        }
+    }
+
     render() {
-        const { loading } = this.state;
-        return <MoviesPresenter loading={loading} />;
+        const { loading, popular, upcoming, nowPlaying } = this.state;
+        // console.log(this.state);
+        return (
+            <MoviesPresenter
+                loading={loading}
+                popular={popular}
+                upcoming={upcoming}
+                nowPlaying={nowPlaying}
+            />
+        );
     }
 }
--- screens/Movies/MoviesPresenter.js
+++ screens/Movies/MoviesPresenter.js
@@ -3,10 +3,13 @@ import { Text } from "react-native";
 import PropTypes from "prop-types";
 import Loader from "../../components/Loader";
 
-const MoviesPresenter = ({ loading }) => loading ? <Loader /> : <Text>Movies</Text>;
+const MoviesPresenter = ({ loading, popular, upcoming, nowPlaying }) => loading ? <Loader /> : <Text>Movies</Text>;
 
 MoviesPresenter.propTypes = {
-    loading: PropTypes.bool.isRequired
+    loading: PropTypes.bool.isRequired,
+    upcoming: PropTypes.array,
+    popular: PropTypes.array,
+    nowPlaying: PropTypes.array
 };
 
 export default MoviesPresenter;
